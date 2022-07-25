0d9f9fe138a60951eadf2e74b9baf66fbc5389a9
erxclau <ericlau41802@gmail.com>
2022-07-25 02:11:51 -0500

    Replace jQuery with awesomplete

--- main.js
+++ main.js
@@ -4,6 +4,16 @@ import { CognitoIdentityClient } from "@aws-sdk/client-cognito-identity";
 import { Signer } from "@aws-amplify/core";
 import { polygonContains } from "d3-polygon";
 
+function debounce(func, timeout = 300) {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(args);
+    }, timeout);
+  };
+};
+
 const getCredentials = async (region, pool) => {
   const provider = fromCognitoIdentityPool({
     client: new CognitoIdentityClient({
@@ -49,6 +59,34 @@ window.onload = async () => {
     -83.6761661429186, 42.3239369094827,
   ];
 
+  const input = document.getElementById("address");
+  const autocomplete = new Awesomplete(input, {
+    filter: () => true, sort: false
+  });
+
+  const search = async () => {
+    if (input.value.length === 0) {
+      return;
+    }
+
+    const command = new SearchPlaceIndexForSuggestionsCommand({
+      IndexName: "ElectionsPollingLocationProductionIndex",
+      FilterBBox: box,
+      MaxResults: 5,
+      Text: input.value,
+    });
+
+    const data = await client.send(command);
+    autocomplete.list = data.Results.map(d => d.Text);
+    autocomplete.evaluate();
+  }
+
+  if ("onsearch" in input) {
+    input.onsearch = search;
+  } else {
+    input.oninput = debounce(search);
+  }
+
   const map = new maplibregl.Map({
     container: "map",
     bounds: box,
@@ -56,110 +94,91 @@ window.onload = async () => {
     hash: false,
     interactive: false,
     transformRequest,
-  })
+  });
 
+  const locationText = document.getElementById("polling-location");
   let addressMarker;
   let pollingMarker;
   let layer;
 
-  const locationText = document.getElementById("polling-location");
+  input.addEventListener("awesomplete-selectcomplete", async ({ text: { value } }) => {
+    const command = new SearchPlaceIndexForTextCommand({
+      IndexName: "ElectionsPollingLocationProductionIndex",
+      FilterBBox: box,
+      MaxResults: 1,
+      Text: value,
+    });
 
-  $("#address").autocomplete({
-    minLength: 3,
-    source: async ({ term }, response) => {
-      if (!term.length) {
-        response([]);
-      }
+    const data = await client.send(command);
 
-      const command = new SearchPlaceIndexForSuggestionsCommand({
-        IndexName: "ElectionsPollingLocationProductionIndex",
-        FilterBBox: box,
-        MaxResults: 5,
-        Text: term,
+    const position = data.Results[0].Place.Geometry.Point;
+    if (addressMarker) {
+      addressMarker.remove();
+    }
+
+    addressMarker = new maplibregl.Marker()
+      .setLngLat(position)
+      .addTo(map);
+
+    const bounds = await import("./geojson/bounds.json");
+    const locations = await import("./geojson/locations.json");
+
+    const bound = bounds.features
+      .filter(f => polygonContains(
+        f.geometry.coordinates.at(0).at(0), position
+      ))
+      .pop();
+
+    const polling = locations.features
+      .filter(f => f.properties.WRDPCT === bound.properties.WRDPCT)
+      .pop();
+
+    let [long, lat] = polling.geometry.coordinates;
+
+    if (pollingMarker) {
+      pollingMarker.remove();
+    }
+
+    pollingMarker = new maplibregl.Marker({ color: "#e34829" })
+      .setLngLat(polling.geometry.coordinates)
+      .addTo(map);
+
+    const address = encodeURIComponent(`${polling.properties.ADDRESS}, Ann Arbor, Michigan`)
+    locationText.textContent = `${polling.properties.PLACE}`;
+    locationText.style.color = "#e34829";
+    locationText.style.fontWeight = "bold";
+    locationText.href = `https://maps.google.com/maps/place/${address}/@${lat},${long}`;
+
+    if (!map.getSource('bound-source')) {
+      map.addSource(`bound-source`, {
+        type: 'geojson',
+        data: bound,
       });
+    } else {
+      map.getSource('bound-source').setData(bound);
+    }
 
-      const data = await client.send(command);
-      response(data.Results.map(d => d.Text));
-    },
-    select: async (_, { item: { value } }) => {
-      const command = new SearchPlaceIndexForTextCommand({
-        IndexName: "ElectionsPollingLocationProductionIndex",
-        FilterBBox: box,
-        MaxResults: 1,
-        Text: value,
-      });
-
-      const data = await client.send(command);
-
-      const position = data.Results[0].Place.Geometry.Point;
-      if (addressMarker) {
-        addressMarker.remove();
-      }
-
-      addressMarker = new maplibregl.Marker()
-        .setLngLat(position)
-        .addTo(map);
-
-      const bounds = await import("./geojson/bounds.json");
-      const locations = await import("./geojson/locations.json");
-
-      const bound = bounds.features
-        .filter(f => polygonContains(
-          f.geometry.coordinates.at(0).at(0), position
-        ))
-        .pop();
-
-      const polling = locations.features
-        .filter(f => f.properties.WRDPCT === bound.properties.WRDPCT)
-        .pop();
-
-      let [long, lat] = polling.geometry.coordinates;
-
-      if (pollingMarker) {
-        pollingMarker.remove();
-      }
-
-      pollingMarker = new maplibregl.Marker({ color: "#e34829" })
-        .setLngLat(polling.geometry.coordinates)
-        .addTo(map);
-
-      const address = encodeURIComponent(`${polling.properties.ADDRESS}, Ann Arbor, Michigan`)
-      locationText.textContent = `${polling.properties.PLACE}`;
-      locationText.style.color = "#e34829";
-      locationText.style.fontWeight = "bold";
-      locationText.href = `https://maps.google.com/maps/place/${address}/@${lat},${long}`;
-
-      if (!map.getSource('bound-source')) {
-        map.addSource(`bound-source`, {
-          type: 'geojson',
-          data: bound,
-        });
-      } else {
-        map.getSource('bound-source').setData(bound);
-      }
-
-      if (!layer) {
-        layer = map.addLayer({
-          id: `bound-layer`,
-          source: `bound-source`,
-          type: 'fill',
-          paint: {
-            "fill-color": "#34aeeb",
-            "fill-opacity": 0.7
-          }
-        });
-      }
-
-      let [swLong, swLat, neLong, neLat] = bound.bbox;
-
-      swLong = Math.min(swLong, long);
-      swLat = Math.min(swLat, lat);
-      neLong = Math.max(neLong, long);
-      neLat = Math.max(neLat, lat);
-
-      map.fitBounds([swLong, swLat, neLong, neLat], {
-        padding: 75,
+    if (!layer) {
+      layer = map.addLayer({
+        id: `bound-layer`,
+        source: `bound-source`,
+        type: 'fill',
+        paint: {
+          "fill-color": "#34aeeb",
+          "fill-opacity": 0.7
+        }
       });
     }
+
+    let [swLong, swLat, neLong, neLat] = bound.bbox;
+
+    swLong = Math.min(swLong, long);
+    swLat = Math.min(swLat, lat);
+    neLong = Math.max(neLong, long);
+    neLat = Math.max(neLat, lat);
+
+    map.fitBounds([swLong, swLat, neLong, neLat], {
+      padding: 75,
+    });
   });
 }
